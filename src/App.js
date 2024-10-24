9a242404e63469cedbc0720c9375d7f43dafd919
Elsultan Kuandyqov <elsultankuandyqov@Elsultans-MacBook-Pro.local>
2024-10-24 14:14:38 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import { useState } from 'react';
+import Input from './components/Input';
+import Board from './components/Board';
+
+function App() {
+  const [taskList,setTaskList] = useState([])
+  return (
+    <>
+    <div className='flex flex-col items-center justify-center py-8 gap-4'>
+      <h1 className='text-xl font-semibold'>02-ToDo-Board</h1>
+      <Input taskList={taskList} setTaskList={setTaskList}/>
+      </div>
+      <div className=' flex flex-col gap-4 sm:grid sm:grid-cols-3 px-4 sm:px-8 md:px-10 lg:px-12'>
+        {taskList.map((task,index)=>
+        <Board key={index} task={task} index = {index} taskList ={taskList} setTaskList={setTaskList}/>
+        )}
+      </div>
+      </> 
+  );
+}
+
+export default App;
